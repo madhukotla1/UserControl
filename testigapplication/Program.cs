8fa2931795ed1a33eecad8fd09e098adb53a357a
madhu kotla <madhu.kotla50@gmail.com>
2016-07-27 00:47:36 -0400

    welcome

--- testigapplication/Program.cs
+++ testigapplication/Program.cs
@@ -10,6 +10,7 @@ namespace testigapplication
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("welcome");
             Console.Write("dotnet");
             Console.Read();
         }
